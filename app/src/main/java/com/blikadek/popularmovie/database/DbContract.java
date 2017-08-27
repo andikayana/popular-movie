bc24aea5c09b6d04bf0cbb65c398fea75b756f61
andikayana <andikayana99@gmail.com>
2017-08-27 12:31:30 +0800

    add save query

--- app/src/main/java/com/blikadek/popularmovie/database/DbContract.java
+++ app/src/main/java/com/blikadek/popularmovie/database/DbContract.java
@@ -12,13 +12,14 @@ public class DbContract {
 
         public static final String TABLE_NAME = "movie_favorite";
 
-        public static final String COLUMN_MOVIE_ID = "movie_id";
-        public static final String COLUMN_TITLE = "title";
-        public static final String COLUMN_POSTER = "poster";
-        public static final String COLUMN_SYNOPSIS = "synopsis";
-        public static final String COLUMN_RATING = "rating";
-        public static final String COLUMN_RELEASE_DATE = "release_date";
-        public static final String COLUMN_BACKDROP = "backdrop";
-        public static final String COLUMN_LANGUAGE = "language";
+        public static final String MOVIE_ID = "movie_id";
+        public static final String TITLE = "title";
+        public static final String POSTER = "poster";
+        public static final String SYNOPSIS = "synopsis";
+        public static final String RATING = "rating";
+        public static final String VOTE_COUNT = "vote_count";
+        public static final String RELEASE_DATE = "release_date";
+        public static final String BACKDROP = "backdrop";
+        public static final String LANGUAGE = "language";
     }
 }
--- app/src/main/java/com/blikadek/popularmovie/database/DbOpenHelper.java
+++ app/src/main/java/com/blikadek/popularmovie/database/DbOpenHelper.java
@@ -1,10 +1,12 @@
 package com.blikadek.popularmovie.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.blikadek.popularmovie.database.DbContract.*;
+import com.blikadek.popularmovie.model.MovieItem;
 
 /**
  * Created by M13x5aY on 27/08/2017.
@@ -23,14 +25,15 @@ public class DbOpenHelper  extends SQLiteOpenHelper{
     private static final String SQL_CREATE_TABEL_MOVIE_FAV =
             "CREATE TABLE " + MovieItemContract.TABLE_NAME + " (" +
                     MovieItemContract._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    MovieItemContract.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
-                    MovieItemContract.COLUMN_TITLE + " TEXT, " +
-                    MovieItemContract.COLUMN_POSTER + " TEXT, " +
-                    MovieItemContract.COLUMN_SYNOPSIS + " TEXT, " +
-                    MovieItemContract.COLUMN_RATING + " REAL, " +
-                    MovieItemContract.COLUMN_RELEASE_DATE + " TEXT, " +
-                    MovieItemContract.COLUMN_BACKDROP + " TEXT " +
-                    MovieItemContract.COLUMN_LANGUAGE + "TEXT " +
+                    MovieItemContract.MOVIE_ID + " INTEGER NOT NULL, " +
+                    MovieItemContract.TITLE + " TEXT, " +
+                    MovieItemContract.POSTER + " TEXT, " +
+                    MovieItemContract.SYNOPSIS + " TEXT, " +
+                    MovieItemContract.RATING + " REAL, " +
+                    MovieItemContract.VOTE_COUNT + " INTEGER, " +
+                    MovieItemContract.RELEASE_DATE + " TEXT, " +
+                    MovieItemContract.BACKDROP + " TEXT " +
+                    MovieItemContract.LANGUAGE + "TEXT " +
                     "); ";
 
     private static final String SQL_DROP_TABEL_MOVIE_FAV =
@@ -48,5 +51,23 @@ public class DbOpenHelper  extends SQLiteOpenHelper{
         db.execSQL(SQL_DROP_TABEL_MOVIE_FAV);
         onCreate(db);
 
+    }
+
+    public long saveMovieItem(MovieItem movieItem){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(MovieItemContract.MOVIE_ID, movieItem.getId());
+        cv.put(MovieItemContract.TITLE, movieItem.getOriginalTitle());
+        cv.put(MovieItemContract.POSTER, movieItem.getPosterPath());
+        cv.put(MovieItemContract.SYNOPSIS, movieItem.getOverview());
+        cv.put(MovieItemContract.RATING, movieItem.getVoteAverage());
+        cv.put(MovieItemContract.VOTE_COUNT, movieItem.getVoteCount());
+        cv.put(MovieItemContract.RELEASE_DATE, movieItem.getReleaseDate());
+        cv.put(MovieItemContract.BACKDROP, movieItem.getBackdropPath());
+        cv.put(MovieItemContract.LANGUAGE, movieItem.getOriginalLanguage());
+
+        return db.insert(MovieItemContract.TABLE_NAME, null, cv);
     }
 }
